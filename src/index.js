5f3041acbc9edb863bd2afd21afca14a68e8986b
Todor Pavlov <tpavlov@softserveinc.com>
2019-06-21 20:54:37 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./index.css";
+// const h = (html) => document.createRange().createContextualFragment(html);
+const tst = 'hi there!'
+import tt from './tst.html'
+import tpl from 'nunjucks'
+
+document.body.innerHTML = tpl.renderString(tt, {tst: 'hi!'});//'<h1>aaaaa</h1>';
+
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// webpack.config.js
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: [':data-src']
+          }
+        }
+      }
+    ]
+  }
+}
