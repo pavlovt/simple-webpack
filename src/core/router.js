76244f8a763aa0c193b9c7f26c2e54789aff8323
Todor Pavlov <pavlovt@gmail.com>
2019-08-22 21:05:52 +0300

    no message

--- src/core/router.js
+++ src/core/router.js
@@ -1,5 +1,30 @@
-class Router {
-  constructor(routes) {}
+import Router from "vanilla-router";
+import homejs from "~/pages/home/home.js";
+import aboutjs from "~/pages/about/about.js";
 
-  goto() {}
-}
+var router = new Router({
+  mode: "history",
+  page404: function(path) {
+    console.log('"/' + path + '" Page not found');
+  }
+});
+
+router.add("", function() {
+  homejs.init();
+});
+
+router.add("home", function() {
+  homejs.init();
+});
+
+router.add("about", function() {
+  aboutjs.init();
+});
+
+router.addUriListener();
+
+router.navigateTo("");
+
+window.router = router;
+
+export default router;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import "./index.css";
 import "./components/header/header";
-// const h = (html) => document.createRange().createContextualFragment(html);
-// const tst = "hi there!";
-// import tt from "./tst.html";
-// import tpl from "nunjucks";
-import "./router";
-// console.log("qqq!!!!!!!!!!", tpl.renderString(tt, { tst: "hi!" }));
-// document.body.innerHTML = tpl.renderString(tt, {tst: 'hi!'});//'<h1>aaaaa</h1>';
-
-// console.log(router)
-// import home from "./pages/home/home";
-// home.init();
+import "./core/router";
